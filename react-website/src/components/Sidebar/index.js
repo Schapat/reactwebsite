d5d4054470bbc15364685bc4efc62c0046684590
Patrick S <68395365+Schapat@users.noreply.github.com>
2022-06-03 17:08:59 +0200

    sidebar

---
+++ react-website/src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { IconBase } from 'react-icons'
+
+function Sidebar() {
+  return (
+    <SidebarContainer>
+      <Icon>
+          <CloseIcon/>
+      </Icon>
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar
