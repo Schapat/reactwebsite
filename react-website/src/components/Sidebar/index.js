79cc1e1e09625e268333593c6bfd836520bd2f01
Patrick S <68395365+Schapat@users.noreply.github.com>
2022-06-03 17:46:56 +0200

    sidebar

--- react-website/src/components/Sidebar/index.js
+++ react-website/src/components/Sidebar/index.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import { IconBase } from 'react-icons'
+import { 
+    SidebarContainer, 
+    Icon, 
+    CloseIcon,
+    SidebarWrapper,
+    SidebarMenu,
+    SidebarLink,
+    SidebarRoute,
+    SideBtnWrap } from './SidebarElements'
 
-function Sidebar() {
+const Sidebar = () => {
   return (
     <SidebarContainer>
       <Icon>
           <CloseIcon/>
       </Icon>
+      <SidebarWrapper>
+          <SidebarMenu>
+              <SidebarLink to='about'>About</SidebarLink>
+              <SidebarLink to='discover'>Discover</SidebarLink>
+              <SidebarLink to='tool'>Tool</SidebarLink>
+              <SidebarLink to='services'>Services</SidebarLink>
+          </SidebarMenu>
+          <SideBtnWrap>
+              <SidebarRoute to='/signin'>Signin</SidebarRoute>
+          </SideBtnWrap>
+      </SidebarWrapper>
     </SidebarContainer>
   )
 }
